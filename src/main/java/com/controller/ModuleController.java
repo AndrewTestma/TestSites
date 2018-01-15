dd7664f870e90656cd92504058734c7598b0264b
AndrewTestma <425780724@qq.com>
2018-01-15 09:40:21 +0800

    添加模块相关代码

---
+++ src/main/java/com/controller/ModuleController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author:Andrew
+ * @Description:
+ * @Date 2018/1/15 0015
+ */
+@Controller
+@RequestMapping("/module")
+public class ModuleController {
+
+}
