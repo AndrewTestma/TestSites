9767ba2793148f798998278c00d3505acc7304a9
AndrewTestma <425780724@qq.com>
2018-02-24 16:40:28 +0800

    增加获取业务列表方法

--- src/main/java/com/controller/BusinessController.java
+++ src/main/java/com/controller/BusinessController.java
@@ -1,10 +1,16 @@
 package com.controller;
 
+import com.pojo.Business;
+import com.service.BusinessService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author:Andrew
@@ -15,7 +21,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/bus")
 public class BusinessController {
     private Logger logger= LoggerFactory.getLogger(this.getClass());
-
+    @Resource(name = "BusinessService")
+    private BusinessService businessService;
     /**
      * @Description:业务管理视图
      * */
@@ -29,5 +36,23 @@ public class BusinessController {
     @RequestMapping(value = "/create",method = RequestMethod.GET)
     public String create(){
         return "/manage/business/create";
+    }
+    /**
+     * @Description：业务列表
+     * */
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    @ResponseBody
+    public Object list(
+            @RequestParam(required = false, defaultValue = "0", value = "offset") int offset,
+            @RequestParam(required = false, defaultValue = "10", value = "limit") int limit,
+            @RequestParam(required = false, defaultValue = "", value = "search") String search,
+            @RequestParam(required = false, value = "sort") String sort,
+            @RequestParam(required = false, value = "order") String order){
+        List<Business> rows=businessService.selectList(offset,limit);
+        long total=rows.size();
+        Map<String,Object> result=new HashMap<>();
+        result.put("data",rows);
+        result.put("total",total);
+        return result;
     }
 }
