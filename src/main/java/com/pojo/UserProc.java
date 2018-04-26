121636daf466dea8f571ec710b039c302b058454
AndrewTestma <425780724@qq.com>
2018-04-26 09:51:04 +0800

    添加用户与产品中间表

---
+++ src/main/java/com/pojo/UserProc.java
@@ -0,0 +1,33 @@
+package com.pojo;
+
+public class UserProc {
+    private Integer tsuserprocid;
+
+    private Integer tsuserid;
+
+    private Integer tsproductid;
+
+    public Integer getTsuserprocid() {
+        return tsuserprocid;
+    }
+
+    public void setTsuserprocid(Integer tsuserprocid) {
+        this.tsuserprocid = tsuserprocid;
+    }
+
+    public Integer getTsuserid() {
+        return tsuserid;
+    }
+
+    public void setTsuserid(Integer tsuserid) {
+        this.tsuserid = tsuserid;
+    }
+
+    public Integer getTsproductid() {
+        return tsproductid;
+    }
+
+    public void setTsproductid(Integer tsproductid) {
+        this.tsproductid = tsproductid;
+    }
+}
